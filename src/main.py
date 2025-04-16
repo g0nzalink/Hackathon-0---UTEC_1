3bc6577417c1b413251a0803e6701c29790d17fb
nik-kotine <mswv280407@gmail.com>
2025-04-15 22:45:52 -0500

    feat: main.py error handling para division, exponenciacion y caracteres

--- src/main.py
+++ src/main.py
@@ -1,9 +1,7 @@
 
 def calculate(text:str) -> float:
-    def exponenciacion(a,b):
-        return float(a)**float(b)
-    
     numeros = text.split()
+    if len(numeros)%2==1: raise SyntaxError
     for i in range(len(numeros)):
         if numeros[i] == "+":
             return suma(numeros[i-1], numeros[i+1])
@@ -13,15 +11,26 @@ def calculate(text:str) -> float:
             return resta(numeros[i-1], numeros[i+1])
         if numeros[i] == "/":
             return division(numeros[i-1], numeros[i+1])
-    
 
-def multiplicacion(a, b ):
-    return float(a)*float(b)
 
+def multiplicacion(a, b):
+    if a.isdigit()==False or b.isdigit()==False: return ValueError
+    return float(a) * float(b)
+
+def suma(a, b):
+    if a.isdigit() == False or b.isdigit() == False: return ValueError
+    return float(a) + float(b)
   
 def division(a, b):
+    if a.isdigit() == False or b.isdigit() == False: return ValueError
+    if b==0: raise ZeroDivisionError
     return float(a) / float(b)
-  
+
 def resta(a,b):
+    if a.isdigit() == False or b.isdigit() == False: return ValueError
     return float(a)-float(b)
 
+def exponenciacion(a,b):
+    if a.isdigit() == False or b.isdigit() == False: return ValueError
+    if a==0 and b==0: raise ArithmeticError
+    return float(a)**float(b)
